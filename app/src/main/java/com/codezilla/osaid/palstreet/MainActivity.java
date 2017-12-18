92f03a4200dcb87363082cd070e62709a098b8a2
osaid <osaid.f1995@gmail.com>
2017-12-18 12:08:33 -0800

    get json from server

--- app/src/main/java/com/codezilla/osaid/palstreet/MainActivity.java
+++ app/src/main/java/com/codezilla/osaid/palstreet/MainActivity.java
@@ -1,10 +1,23 @@
 package com.codezilla.osaid.palstreet;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +30,108 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+
+
+        String url = "http://10.0.2.2/palstreet/public/get-news";
+        DownloadTextTask runner = new DownloadTextTask();
+        runner.execute(url);
+
+
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(48.85819, 2.29458))
+                        .title("Eiffel Tower")
+                );
+            }
+        });
+
+
+
+
     }
+
+    private InputStream OpenHttpConnection(String urlString) throws IOException
+    {
+        InputStream in = null;
+        int response = -1;
+
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+
+        if (!(conn instanceof HttpURLConnection))
+            throw new IOException("Not an HTTP connection");
+        try{
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            response = httpConn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.d("Networking", ex.getLocalizedMessage());
+            throw new IOException("Error connecting");
+        }
+        return in;
+    }
+
+    private String DownloadText(String url){
+        int BUFFER_SIZE=2000;
+        InputStream in = null;
+
+        try {
+            in = OpenHttpConnection(url);
+        } catch (IOException ex) {
+            Log.d("NetWorking", ex.getLocalizedMessage());
+            return "";
+        }
+
+        InputStreamReader isr = new InputStreamReader(in);
+        int charRead;
+        String string = "";
+        char[] inputBuffer = new char[BUFFER_SIZE];
+
+        try{
+            while ((charRead = isr.read(inputBuffer))>0){
+                String readString = String.copyValueOf(inputBuffer, 0, charRead);
+                string +=readString;
+                inputBuffer = new char[BUFFER_SIZE];
+            }
+            in.close();
+        }catch (IOException ex){
+            Log.d("NetWorking", ex.getLocalizedMessage());
+            return "";
+        }
+
+        return string;
+    }
+
+
+    private class DownloadTextTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            return DownloadText(urls[0]);
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            //Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG).show();
+            String[] books = result.split(",");
+            String str = "";
+            for(String s : books){
+                str+= s + "\n";
+            }
+
+            Log.e("myeshe",str);
+        }
+    }
+
 
     @Override
     public void onStart() {
